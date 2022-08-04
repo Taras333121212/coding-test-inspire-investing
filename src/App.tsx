ab05359cb8e51a5cf55ae9cfbbc631c24c690b11
Waldemar <chooter1999@gmail.com>
2022-08-04 16:22:48 +0300

    test task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import styles from './App.module.css'
 
 function App() {
+  const [holdings, setHoldings] = useState<any>([])
+
+  const fetchData = async () => {
+    const data = await axios.get('https://api.inspireinsight.com/api/tickers/1784/constituents?size=100')
+    setHoldings(data.data.holdings)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (holdings.length === 0) {
+    return (
+      <>
+        <div className={styles['lds-hourglass']}></div>
+      </>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.holdings}>
+      {holdings.map((holding: any) => (
+        <div className={styles.holding} key={holding.financialInstrumentId}>
+        <ul>
+          <li>Name: {holding.name}</li>
+          <li>Ticker: {holding.ticker}</li>
+          <li>Score: {holding.score}</li>
+          <li>Holding Percentage: {holding.holdingPercentage}</li>
+        </ul>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
